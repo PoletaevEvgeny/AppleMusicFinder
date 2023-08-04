d14de2fbb98e571699c6e406cb9a9003a727e442
PoletaevEvgeny <poletaevev89@gmail.com>
2023-08-05 00:11:02 +0300

    First commit

---
+++ AppleMusicFinder/AppleMusicFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppleMusicFinder
+//
+//  Created by Евгений Полетаев on 04.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
