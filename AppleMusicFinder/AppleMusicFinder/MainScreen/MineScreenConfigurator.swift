eca555ec0f67f98d7ef91c4cdc0fdd3724991fde
PoletaevEvgeny <poletaevev89@gmail.com>
2023-08-18 09:44:10 +0300

    created an MVP template

---
+++ AppleMusicFinder/AppleMusicFinder/MainScreen/MineScreenConfigurator.swift
@@ -0,0 +1,8 @@
+//
+//  MineScreenConfiigurator.swift
+//  AppleMusicFinder
+//
+//  Created by Евгений Полетаев on 17.08.2023.
+//
+
+import Foundation
---
+++ AppleMusicFinder/AppleMusicFinder/MusicTrack.swift
@@ -0,0 +1,8 @@
+//
+//  MusicTrack.swift
+//  AppleMusicFinder
+//
+//  Created by Евгений Полетаев on 18.08.2023.
+//
+
+import Foundation
